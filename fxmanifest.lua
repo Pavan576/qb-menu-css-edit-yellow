9601e7cdfe63074d414cd0458d208c63a7cba86f
Munna Dev <49777725+Pavan576@users.noreply.github.com>
2022-07-04 20:33:45 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'QBCore'
+description 'QB-Menu CSS Edit By Munna Dev..'
+version 'v2'
+
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css'
+}
+
+lua54 'yes'
